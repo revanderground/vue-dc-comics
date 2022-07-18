6ae7cea386cf0fba8e505728b30dc41a0d02aa4a
vtnboolean <vnthuanx@gmail.com>
2022-07-18 05:23:26 +0200

    FooterTop fixed

--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -54,147 +54,120 @@ export default {
                 {
                     text: 'Characters',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Comics',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Movies',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Tv',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Games',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Collectibles',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Videos',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Fans',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'News',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Shop',
                     url: '#',
-                    current: false,
                 }
             ],
             shopLinks: [
                 {
                     text: 'Shop DC',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Shop DC Collectibles',
                     url: '#',
-                    current: false,
                 }
             ],
             dcLinks: [
                 {
                     text: 'Term of Use',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Privacy policy (New)',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Ad Choices',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Advertising',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Jobs',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Subscriptions',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Talent Workshop',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'CPSC Certificates',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Ratings',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Shop Help',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'Contact Us',
                     url: '#',
-                    current: false,
+
                 }
             ],
             sitesLinks: [
                 {
                     text: 'DC',
                     url: '#',
-                    current: false,
                 },{
                     text: 'MAD Magazine',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'DC Kids',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'DC Universe',
                     url: '#',
-                    current: false,
                 },
                 {
                     text: 'DC Power Visa',
                     url: '#',
-                    current: false,
                 },
             ]
         }
